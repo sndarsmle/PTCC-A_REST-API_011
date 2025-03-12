522108a358b3bfaa280cf1574b85282292cada8f
sndarsmle <rizsanaa23@gmail.com>
2025-03-13 03:07:12 +0700

    migrasi

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('tcc_t2', 'root', '', {
-    host : 'localhost',
+const db = new Sequelize('tcc_t2', 'root', 'lingkarankecil', {
+    host : '130.211.201.103',
     dialect : 'mysql'
 });
 
